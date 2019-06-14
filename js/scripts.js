86327ae0a7a7fe034766d9db1cef4090632e58e5
Kirsten Putman <kirstenputman@Kirstens-MacBook-Pro.local>
2019-06-14 13:14:33 -0700

    additions and re-work to scripts

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+var beep = function(num){
+var arr = [];
+for(var i = 0; i <= num; i++){
+var str = i.toString();
+
+
+if(i = 0 && i % 3 === 0 ){
+   arr.push("I'm sorry, Dave. I'm afraid I can't do that.");
+ }
+    else if( str === "0"){
+      arr.push( "Boop!");
+    }
+    else if( str ==  "1"){
+      arr.push("Beep!");
+    }
+
+    else{
+      arr.push(i);
+    }
+
+}
+return (arr);
+
+}
+$(document).ready(function(){
+  $("#formOne").on("submit",function(event){
+
+    var InPut = $("#beep-boop").val();
+    event.preventDefault();
+
+    var bop = beep(InPut);
+    $("#outPut").text(bop);
+  });
+});
