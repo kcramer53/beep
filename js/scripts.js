b561b0d829acb35a95fc97e1fa9cb878316a91b3
Kirsten Putman <kirstenputman@Kirstens-MacBook-Pro.local>
2019-06-14 16:22:47 -0700

    added boop definition

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 //business logic//
 var beep = function(num){
+var boop = (num);
 var arr = [];
 for(var i = 0; i <= num; i++){
   console.log(num);
@@ -11,30 +12,24 @@ for(var i = 0; i <= num; i++){
   else if (i.toString().includes("1")){
   arr.push("Boop");
 }
-    else if(i = 0 && i % 3 === 0 ){
+  else if (i % 3 === 0){
    arr.push("I'm sorry, Dave. I'm afraid I can't do that.");
  }
-    else if( str === "0"){
-      arr.push( "Boop!");
-    }
-    else if( str ==  "1"){
-      arr.push("Beep!");
-    }
+
 
     else {
-      arr.push(i);
+      arr.push(' ' + i);
     }
+    return (arr);
+}
 
-}
-return (arr);
-}
+
 //user interface logic//
 $(document).ready(function(){
   $("form#formOne").on("submit",function(event){
     event.preventDefault();
-    var InPut = $("#beep-boop").val();
-    var bop = beep(InPut);
-    $("#outPut").text(bop);
+    var InPut = $("#formOne").val();
+    var boop = beep(InPut);
+    $("#output").text(boop);
   });
-});
 });
