e06997e96fb75a04766213b8c119835b0f8c8fb6
Kirsten Putman <kirstenputman@Kirstens-MacBook-Pro.local>
2019-06-14 15:35:49 -0700

    changed scripts code

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    event.preventDefault();
+//business logic//
 var beep = function(num){
 var arr = [];
 for(var i = 0; i <= num; i++){
   console.log(num);
-var str = i.toString();
+}
+  if (i.toString().includes("0")){
+  arr.push("Beep");
 }
 
-if(i = 0 && i % 3 === 0 ){
+  else if (i.toString().includes("1")){
+  arr.push("Boop");
+}
+    else if(i = 0 && i % 3 === 0 ){
    arr.push("I'm sorry, Dave. I'm afraid I can't do that.");
  }
     else if( str === "0"){
@@ -18,20 +21,18 @@ if(i = 0 && i % 3 === 0 ){
       arr.push("Beep!");
     }
 
-    else{
+    else {
       arr.push(i);
     }
 
 }
 return (arr);
-
-});
+}
+//user interface logic//
 $(document).ready(function(){
-  $("#formOne").on("submit",function(event){
-
-    var InPut = $("#beep-boop").val();
+  $("form#formOne").on("submit",function(event){
     event.preventDefault();
-
+    var InPut = $("#beep-boop").val();
     var bop = beep(InPut);
     $("#outPut").text(bop);
   });
